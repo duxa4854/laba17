ebeca02e449594123e804f6b5d26b500ea2121cc
duxa4854 <nzaicenko531@gmail.com>
2026-03-17 14:12:49 +0300

    Step 6: while and for loops with flowecharts

---
+++ code/Step6_MultiplicationTable/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Таблицв умножения");
+Console.Write("Введите число для таблицы умножения");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Таблица умножения дл {number}: ");
+for (int i = 1; i <= 10; i++)
+{
+    int result = number * i;
+    System.Console.WriteLine($"{number}x {i} = {result}");
+}
+Console.WriteLine("Конец программы");
